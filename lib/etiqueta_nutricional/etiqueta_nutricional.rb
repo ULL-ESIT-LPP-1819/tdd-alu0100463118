56e3969a9fe8bdbd33f7010df3d023ef512ed7dc
alu0100463118 <alu0100463118@ull.edu.es>
2018-11-06 09:59:53 +0000

    Se crea la clase pasandole los parametros

--- lib/etiqueta_nutricional/etiqueta_nutricional.rb
+++ lib/etiqueta_nutricional/etiqueta_nutricional.rb
@@ -1,6 +1,15 @@
 class Etiqueta
   
-  def initialize()
+  attr_reader :valor_energetico, :grasas, :grasas_saturadas, :hidratos_de_carbono, :azucares, :proteinas, :sal
+  
+  def initialize(valor_energetico, grasas, grasas_saturadas, hidratos_de_carbono, azucares, proteinas, sal)
+    @valor_energetico = valor_energetico
+    @grasas = grasas
+    @grasas_saturadas = grasas_saturadas
+    @hidratos_de_carbono = hidratos_de_carbono
+    @azucares = azucares
+    @proteinas = proteinas
+    @sal = sal
   end
   
 end
