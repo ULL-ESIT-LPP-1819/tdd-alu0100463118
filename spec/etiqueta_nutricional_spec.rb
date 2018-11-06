4a409bced2c081d77df2cf0f953c80d6c3484c01
alu0100463118 <alu0100463118@ull.edu.es>
2018-11-06 10:04:29 +0000

    Añadida la espectativa debe tener un nombre la etiqueta

--- spec/etiqueta_nutricional_spec.rb
+++ spec/etiqueta_nutricional_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe EtiquetaNutricional do
   end
 
   it "Existe la clase etiqueta nutricional" do
-    @etiqueta1 = Etiqueta.new(10,10,10,10,10,10,10)
+    @etiqueta1 = Etiqueta.new("nombre", 10, 10, 10, 10, 10, 10, 10)
+  end
+  
+  it "Debe existir un nombre para la etiqueta" do
+    expect(@etiqueta1.nombre).to eq("nombre")
   end
 end
