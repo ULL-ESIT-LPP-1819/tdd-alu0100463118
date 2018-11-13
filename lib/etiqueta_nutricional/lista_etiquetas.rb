bb91853999707cd8a8be8d4f618fbb9d19ca9756
alu0100463118 <alu0100463118@ull.edu.es>
2018-11-13 14:24:11 +0000

    Se puede crear una lista de etiquetas

--- lib/etiqueta_nutricional/lista_etiquetas.rb
+++ lib/etiqueta_nutricional/lista_etiquetas.rb
@@ -5,8 +5,19 @@ class ListaEtiquetas
   include Enumerable
   
   def initialize()
-        @inicio = Nodo.new(nil)
-        @fin = Nodo.new(nil)
+    @inicio = Nodo.new(nil)
+    @fin = Nodo.new(nil)
   end 
+  
+  def pushI(nodo1)
+    nodo = Nodo.new(nodo1, nil, nil)
+    if @inicio == nil then
+      @inicio = nodo
+      @fin = nodo
+    else
+      nodo.next = @inicio
+      @inicio = nodo 
+    end
+  end
   
 end
