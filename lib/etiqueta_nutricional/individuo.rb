4f0aa25f51e0b19a7028778eada59e09d6cb46ef
alu0100463118 <alu0100463118@ull.edu.es>
2018-11-21 06:15:07 +0000

    Se muestra correctamente un individuo

--- lib/etiqueta_nutricional/individuo.rb
+++ lib/etiqueta_nutricional/individuo.rb
@@ -7,4 +7,22 @@ class Individuo
         @edad = edad
         @sexo = sexo
     end
+    
+    def obtener_nombre
+        @nombre
+    end
+    
+    def to_s
+        cadena = ""
+        sexo = ""
+        cadena = cadena + "Nombre: #{@nombre}\n"
+        cadena = cadena + "Edad: #{@edad}\n"
+        if @sexo == 0
+            sexo = "Hombre"
+        else
+            sexo = "Mujer"
+        end
+        cadena = cadena + "Sexo: #{sexo}\n"
+        return cadena
+    end
 end
